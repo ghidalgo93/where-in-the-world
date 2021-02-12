de1c604cf19eaaf8f36c7bf5b40d28b764f4a16b
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2021-02-11 17:24:51 -0700

    adding crosshairs to cursor

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import n64 from "../images/pierre-roussel-n64-phone2.jpg";
 
 function Home() {
   return (
-    <img className={"content"} src={n64} alt="n64 expolded with characters" />
+    <img
+      className={"img-pzl content"}
+      src={n64}
+      alt="n64 expolded with characters"
+    />
   );
 }
 
