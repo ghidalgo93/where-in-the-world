255b5aa404e7d8f32444744e5f52cd73e4564caf
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2021-02-17 13:06:30 -0700

    get data back from firebase

--- src/components/NamesPopup.js
+++ src/components/NamesPopup.js
@@ -1,46 +1,57 @@
 import React, { useState, useEffect } from "react";
+import { useAsync } from "react-async";
 import "../styles/App.css";
 import firebase from "firebase/app";
 
 const getData = async () => {
-  const query = firebase.firestore().collection("chars");
-  const snapshot = await query.get();
-  return snapshot.docs.map((doc) => {
-    return doc.data();
-  });
+  try {
+    const query = firebase.firestore().collection("chars");
+    const snapshot = await query.get();
+    return snapshot.docs.map((doc) => {
+      return doc.data();
+    });
+  } catch (error) {
+    console.log("Collection not found: ", error);
+  }
+};
+
+const CharList = (props) => {
+  const { data, error } = useAsync({ promiseFn: getData });
+  const handleClick = (e) => {
+    console.log(props.pos);
+    console.log(data);
+    // console.log(data);
+    // if the clicked name has coords within range
+    // make success note
+    // remove name from list
+    // else
+    // error note
+  };
+
+  if (error) return error.message;
+  if (data) {
+    const listItems = data.map((char) => (
+      <li key={char.id} className={"name"} onClick={handleClick}>
+        {char.name}
+      </li>
+    ));
+    const list = (
+      <ul className={"modal names"} style={props.pos}>
+        {listItems}
+      </ul>
+    );
+    return list;
+  }
+  return null;
 };
 
 const NamesPopup = (props) => {
-  const [selected, setSelected] = useState(null);
-  let chars;
-  useEffect(async () => {
-    chars = await getData();
-  }, []);
-
-  const handleClick = (e) => {
-    console.log(e.target);
-    console.log(chars);
-    // check to see if the character selected is within range of click
-  };
-
   const positionStyle = {
     top: props.coords[1] + 20,
     left: props.coords[0] + 20,
   };
 
-  return (
-    <ul className={"modal names"} style={positionStyle}>
-      <li className={"name"} onClick={handleClick}>
-        name1
-      </li>
-      <li className={"name"} onClick={handleClick}>
-        name2
-      </li>
-      <li className={"name"} onClick={handleClick}>
-        name3
-      </li>
-    </ul>
-  );
+  return <CharList pos={positionStyle} />;
 };
 
 export default NamesPopup;
