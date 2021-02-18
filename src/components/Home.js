562cd1a0d568c0d8844ed443152757e9349611c6
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2021-02-18 12:35:45 -0700

    refactor db call to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from "react";
+import { useAsync } from "react-async";
 import "../styles/App.css";
 import TargetPopup from "./TargetPopup";
 import NamesPopup from "./NamesPopup";
 import n64 from "../images/pierre-roussel-n64-phone2.jpg";
 import { getCursorXY } from "../helpers";
+import firebase from "firebase/app";
 
 function getElementXY() {
   const xOffset = window.event.offsetX;
   const yOffset = window.event.offsetY;
   return { x: xOffset, y: yOffset };
 }
+
+const getData = async () => {
+  try {
+    const query = firebase.firestore().collection("chars");
+    const snapshot = await query.get();
+    return snapshot.docs.map((doc) => {
+      return doc.data();
+    });
+  } catch (error) {
+    console.log("Collection not found: ", error);
+  }
+};
 
 const Home = () => {
   const [imgClickLocation, setImgClickLocation] = useState({
@@ -25,18 +39,27 @@ const Home = () => {
     setImgClickLocation({ x: getElementXY().x, y: getElementXY().y });
     toggleTargetShown();
   };
-  return (
-    <div className={"img-container"}>
-      {targetShown ? <TargetPopup coords={imgClickLocation} /> : null}
-      {targetShown ? <NamesPopup coords={imgClickLocation} /> : null}
-      <img
-        className={"img-pzl content"}
-        src={n64}
-        alt="n64 expolded with characters"
-        onClick={handleClick}
-      />
-    </div>
-  );
+
+  //get data ascyjdlafly
+  const { data, error } = useAsync({ promiseFn: getData });
+  if (error) return `Something went wrong: ${error}`;
+  if (data) {
+    return (
+      <div className={"img-container"}>
+        {targetShown ? <TargetPopup coords={imgClickLocation} /> : null}
+        {targetShown ? (
+          <NamesPopup coords={imgClickLocation} data={data} />
+        ) : null}
+        <img
+          className={"img-pzl content"}
+          src={n64}
+          alt="n64 expolded with characters"
+          onClick={handleClick}
+        />
+      </div>
+    );
+  }
+  return null;
 };
 
 export default Home;
--- src/components/NamesPopup.js
+++ src/components/NamesPopup.js
@@ -3,18 +3,6 @@ import { useAsync } from "react-async";
 import "../styles/App.css";
 import firebase from "firebase/app";
 
-const getData = async () => {
-  try {
-    const query = firebase.firestore().collection("chars");
-    const snapshot = await query.get();
-    return snapshot.docs.map((doc) => {
-      return doc.data();
-    });
-  } catch (error) {
-    console.log("Collection not found: ", error);
-  }
-};
-
 const NamesPopup = (props) => {
   const positionStyle = {
     left: props.coords.x + 20,
@@ -24,24 +12,21 @@ const NamesPopup = (props) => {
   const handleClick = (e) => {
     console.log(e.target);
     console.log(props.coords);
+    //logic time budddddddy!
+    //
+    //
   };
+  const listItems = props.data.map((char) => (
+    <li key={char.id} className={"name"} onClick={handleClick}>
+      {char.name}
+    </li>
+  ));
 
-  //get the data asynchronoskasdlkfly
-  const { data, error } = useAsync({ promiseFn: getData });
-  if (error) return `Someting went wrong: ${error}`;
-  if (data) {
-    const listItems = data.map((char) => (
-      <li key={char.id} className={"name"} onClick={handleClick}>
-        {char.name}
-      </li>
-    ));
-    return (
-      <ul className={"modal names"} style={positionStyle}>
-        {listItems}
-      </ul>
-    );
-  }
-  return null;
+  return (
+    <ul className={"modal names"} style={positionStyle}>
+      {listItems}
+    </ul>
+  );
 };
 
 export default NamesPopup;
